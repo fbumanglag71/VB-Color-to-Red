fd1760b1fdffecde46c027cd6f2ce42e34384189
franc <franc@BLUE>
2024-06-23 08:51:35 -0700

    Add project files.

---
+++ WinFormsApp8/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnRed = New System.Windows.Forms.Button()
+        Me.txtFrst = New System.Windows.Forms.TextBox()
+        Me.txtSecond = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnRed
+        '
+        Me.btnRed.Location = New System.Drawing.Point(147, 153)
+        Me.btnRed.Name = "btnRed"
+        Me.btnRed.Size = New System.Drawing.Size(278, 23)
+        Me.btnRed.TabIndex = 0
+        Me.btnRed.Text = "Change Color To Red"
+        Me.btnRed.UseVisualStyleBackColor = True
+        '
+        'txtFrst
+        '
+        Me.txtFrst.Location = New System.Drawing.Point(147, 49)
+        Me.txtFrst.Name = "txtFrst"
+        Me.txtFrst.Size = New System.Drawing.Size(278, 23)
+        Me.txtFrst.TabIndex = 1
+        '
+        'txtSecond
+        '
+        Me.txtSecond.Location = New System.Drawing.Point(147, 103)
+        Me.txtSecond.Name = "txtSecond"
+        Me.txtSecond.Size = New System.Drawing.Size(278, 23)
+        Me.txtSecond.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(574, 255)
+        Me.Controls.Add(Me.txtSecond)
+        Me.Controls.Add(Me.txtFrst)
+        Me.Controls.Add(Me.btnRed)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnRed As Button
+    Friend WithEvents txtFrst As TextBox
+    Friend WithEvents txtSecond As TextBox
+End Class
---
+++ WinFormsApp8/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.Text = "Demo"
+
+    End Sub
+
+    Private Sub BttnPush(sender As Object, e As EventArgs) Handles btnRed.Click
+
+        txtFrst.ForeColor = Color.Red
+
+    End Sub
+
+    Private Sub txtFrst_TextChanged(sender As Object, e As EventArgs) Handles txtFrst.TextChanged
+
+        txtFrst.ForeColor = Color.Blue
+
+    End Sub
+
+    Private Sub txtFrst_Leave(sender As Object, e As EventArgs) Handles txtFrst.Leave
+
+        txtFrst.ForeColor = Color.Black
+
+    End Sub
+End Class
